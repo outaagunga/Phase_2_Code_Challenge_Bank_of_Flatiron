e94b09f0773044e7167504ffdff24a0040b18a18
outaagunga <jeconiaagunga@gmail.com>
2023-07-29 04:13:12 +0300

    Implementing the search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,29 @@
-import react from "react";
-function SearchBar() {
+import React, { useState } from "react";
+
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(searchTerm);
+  };
+
   return (
-    <>
-      <div>
-        <p>As soon as possible</p>
-      </div>
-    </>
+    <div className="my-4">
+      <input
+        type="text"
+        placeholder="Search by ID, Date, Description, Category, or Amount..."
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+      <button className="btn btn-primary ml-2" onClick={handleSearch}>
+        Search
+      </button>
+    </div>
   );
-}
+};
 
 export default SearchBar;
--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,26 +1,66 @@
-import { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import SearchBar from "./SearchBar"; // Replaced "TransactionSearchBar" with "SearchBar"
 
 const TransactionList = () => {
-  const [transactions, setTransaction] = useState([]);
+  const [transactions, setTransactions] = useState([]);
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8001/transactions")
-      .then((response) => response.json())
-      .then((transaction) => setTransaction(transaction))
-      .catch((error) => console.error("Error fetching data:", error));
+    fetchTransactions();
   }, []);
 
+  const fetchTransactions = async () => {
+    try {
+      const response = await fetch("http://localhost:8001/transactions");
+      const data = await response.json();
+      setTransactions(data);
+      setFilteredTransactions(data); // Initially, show all transactions
+    } catch (error) {
+      console.error("Error fetching transactions:", error);
+    }
+  };
+
+  const handleSearch = (searchTerm) => {
+    if (searchTerm.trim() === "") {
+      setFilteredTransactions(transactions); // If search term is empty, show all transactions
+    } else {
+      const filteredData = transactions.filter((transaction) =>
+        Object.values(transaction).some((field) =>
+          field.toString().toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      );
+      setFilteredTransactions(filteredData);
+    }
+  };
+
   return (
-    <>
-      <div>
-        {transactions.map((transaction) => (
-          <div key={transaction.id}>
-            {transaction.id}, {transaction.date}, {transaction.description},{" "}
-            {transaction.category}, {transaction.amount}
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="container mt-4">
+      <h1>Transaction Data</h1>
+      <h3>Here are the transaction details for the Flatiron Bank</h3>
+      <SearchBar onSearch={handleSearch} />
+      <table className="table table-bordered table-hover">
+        <thead className="thead-dark">
+          <tr>
+            <th>ID</th>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredTransactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.id}</td>
+              <td>{transaction.date}</td>
+              <td>{transaction.description}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
